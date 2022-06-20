33c0ef4bbc783c0bbe594b0c07a996efbf04fc98
dhmk47 <dhmk47@naver.com>
2022-06-20 21:47:03 +0900

    javascript 수정

--- js/event.js
+++ js/event.js
@@ -1,24 +1,37 @@
-const inputBox = document.querySelector(".input-box form input");
+// input 태그
+const inputBox = document.querySelector(".input-box input");
+
+// input태그의 조상
 const headerInputBox = document.querySelector(".header-input-box");
+
+// input박스 안에 있는 작은 div태그
 const smallBox = document.querySelector(".small-box");
+
+// 작은 div안에 있는 span 태그
 const spanBox = document.querySelector(".small-box span");
-const addDivBox = document.querySelector(".if-check-add-readme-box");
+
+// checkbox type의 input 태그
 const checkbox = document.querySelector("#add-readme-file-box");
 
+// 위 checkbox를 체크하면 나타날 div 박스
+const addDivBox = document.querySelector(".if-check-add-readme-box");
+
 inputBox.onfocus = () => {
+    // 흰바탕으로 변경
     headerInputBox.classList.toggle("change-header-input-box");
     inputBox.classList.toggle("change-input-box");
+
+    // smallBox 숨기기
     smallBox.classList.toggle("remove-small-box");
-    spanBox.classList.toggle("remove-span-box");
 }
 
 inputBox.onblur = () => {
     headerInputBox.classList.toggle("change-header-input-box");
     inputBox.classList.toggle("change-input-box");
     smallBox.classList.toggle("remove-small-box");
-    spanBox.classList.toggle("remove-span-box");
 }
 
 checkbox.onchange = () => {
+    // 숨긴 div박스 나타내기
     addDivBox.classList.toggle("add-div-box");
 }
