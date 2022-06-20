ddabd8b80b85314f22f43877fb18ff237e0c2240
dhmk47 <dhmk47@naver.com>
2022-06-20 16:44:35 +0900

    checkbox 선택시 javascript로 기능 구현

--- js/event.js
+++ js/event.js
@@ -2,6 +2,8 @@ const inputBox = document.querySelector(".input-box form input");
 const headerInputBox = document.querySelector(".header-input-box");
 const smallBox = document.querySelector(".small-box");
 const spanBox = document.querySelector(".small-box span");
+const addDivBox = document.querySelector(".if-check-add-readme-box");
+const checkbox = document.querySelector("#add-readme-file-box");
 
 inputBox.onfocus = () => {
     headerInputBox.classList.toggle("change-header-input-box");
@@ -15,4 +17,8 @@ inputBox.onblur = () => {
     inputBox.classList.toggle("change-input-box");
     smallBox.classList.toggle("remove-small-box");
     spanBox.classList.toggle("remove-span-box");
+}
+
+checkbox.onchange = () => {
+    addDivBox.classList.toggle("add-div-box");
 }
